e0714c70a4f6d6e347e7d5141fc266939c41a25e
soumyaevan <sensoumya94@gmail.com>
2020-09-12 10:52:24 +0200

    Added Update user account functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Regexp, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -26,3 +28,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Passsword', validators=[DataRequired(), Length(min=3, max=15)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20), 
+                            Regexp("^[a-zA-Z]+[\._-]?[a-zA-Z0-9]+")])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username already exists. Please choose a different one')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email address already exists. Please choose a different one')
