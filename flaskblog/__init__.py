9d6f68f5fde44e0db793cabd072f8fa1285e7f94
soumyaevan <sensoumya94@gmail.com>
2020-09-14 19:13:57 +0200

    Added configuration file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,18 +9,12 @@ from flaskblog.config import Config
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='441419fab395443e1a8b5141d59f1c77'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view  = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('DEV_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('DEV_PASSWORD')
 mail = Mail(app)
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
